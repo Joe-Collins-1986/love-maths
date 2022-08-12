c4302f3396a6fd7b45673c13dab73ba8f459fd36
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-08-12 17:01:10 +0000

    failed to save before last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    
+
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
@@ -17,10 +17,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
+    // Create two numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 
 }
+
 
 function checkAnswer() {
 
