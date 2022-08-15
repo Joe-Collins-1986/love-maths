80567cee4334cd1f8584fbbba5f35e333949b646
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-08-15 15:23:08 +0000

    add flavicon, and final tidy ops for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }        
+    })
+
     runGame("addition");
 });
 
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Create two numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
@@ -35,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract")  {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division")  {
+        displayDivisionQuestion(num1, num2);    
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type ${gameType}.Aborting`;
@@ -80,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "%") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator: ${operator}.Aborting!`;
@@ -128,6 +141,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivisionQuestion() {
+function displayDivisionQuestion(operand1, operand2) {
 
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "%";
 }
